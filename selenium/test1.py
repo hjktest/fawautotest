e0d69f90fab17242ec083388d1e6985d1893e123
hjktest <648073731@qq.com>
2018-08-23 14:56:57 +0800

    faw 2018-08-23

---
+++ selenium/test1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author:houjinkai
+# datetime:2018/8/21 13:06
+# software: PyCharm
+class DemoClass2:
+    @classmethod
+    def classPrint(self):
+        print("class method")
+    def objPrint(self):
+        print("obj method")
+
+class DemoClass3:
+    @staticmethod
+    #不需要传入参数
+    def classPrint():
+        print("class method")
+    def objPrint(self):
+        print("obj method")
+class DemoClass1:
+    def classPrint(self):
+        print("class method")
+    def objPrint(self):
+        print("obj method")
+# DemoClass2.classPrint()
+# obj=DemoClass2()
+# obj.classPrint()
+# DemoClass3.classPrint()
+obj=DemoClass1()
+#通过类条用函数
+DemoClass1.classPrint(obj)
+#通过实例调用函数
+obj.classPrint()
+obj.objPrint()
