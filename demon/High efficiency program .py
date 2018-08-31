b4540a5f6bacbd6b725bf66b23042b1191135b92
hjktest <648073731@qq.com>
2018-08-31 15:44:55 +0800

    High efficiency2018年8月31日  15:44

---
+++ demon/High efficiency program .py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author:houjinkai
+# datetime:2018/8/31 15:02
+# software: PyCharm
+from itertools import combinations
+# for x in range(1,101):
+#     print "three"[x%3*len('three')::]+"five"[x%5*len("five")::] or x
+nfc=["packers","49ers"]
+afc=["Ravens","Patriots"]
+for teama,teamb in zip(nfc,afc):
+    print teama + " vs." +teamb
+# 带索引的列表迭代
+teams=["Packers", "49ers", "Ravens", "Patriots"]
+for index,team in enumerate(teams):
+    print index,team
+#查找所有的组合
+teams=["bananas","orange","pair"]
+for fruit in combinations(teams,2):
+    print fruit
--- demon/fact.py
+++ demon/fact.py
@@ -8,5 +8,6 @@ def fact(n):
         return 1
     else:
         return fact(n-1)*n
+
 if __name__ == "__main__":
     print fact(5)
