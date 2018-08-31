52748c78334d5b16972174f69ff5a49ea5cbdf2e
hjktest <648073731@qq.com>
2018-08-31 14:24:05 +0800

    assert-method 2018年8月31日 13 26

--- selenium/assert_method.py
+++ selenium/assert_method.py
@@ -31,6 +31,8 @@ class demoTest(unittest.TestCase):
         self.assertNotIn(11,range(5))
     def test7(self):
         self.assertGreater(7,6,msg="真的大于")
+
+    # 判断a与b的对象是否相同，成立则True，否则False
     def test8(self):
         self.assertIs(1,1,"内容不一致")
     def test9(self):
@@ -51,6 +53,40 @@ class demoTest(unittest.TestCase):
         self.assertAlmostEqual(5, 2, delta=2)
         self.assertAlmostEqual(2, 2.005, places=1)
         self.assertAlmostEqual(2, 2.05, places=3)
+        #self.assertNotAlmostEqual(a, b, delta=c)#a不等于b 同时 a-b>c 则正确，否则错误
+        self.assertNotAlmostEqual(2,2,places=2)
+        # self.assertDictEqual(a, b)  # 判断字典a和字典b是否相等，a,b为字典
+
+        # 注：sorted排序，方法内部为，将a, b分别list，生成各自列表，在sorted排序在比对
+        #
+        # self.assertMultiLineEqual(a, b)  # 比较a文本与b文本是否一致，即便多了个换行，也会区分
+        #
+        # self.assertLess(a, b)  # 判断a<b 成立则通过，否则失败
+        #
+        # self.assertLessEqual  # 判断a<=b 成立则通过，否则失败
+        #
+        # self.assertGreater  # 判断a>b 成立则通过，否则失败
+        #
+        # self.assertGreaterEqual  # 判断a>=b 成立则通过，否则失败
+        #
+        # self.assertIsNone(obj=””)  # 判断obj=None 成立则通过，否则失败
+        #
+        # self.assertIsNotNone  # 判断obj=None 成立则失败，否则通过
+        #
+        # self.assertIsInstance(a, b)  # 判断a的数据类型是否为b，isinstance(a,b) 成立则通过，否则失败
+        #
+        # self.assertNotIsInstance  # 判断同上相反
+        #
+        # self.assertRaisesRegexp  # 正则判断匹配，没仔细看，过程复杂
+        #
+        # self.assertRegexpMatches(a, b)  # 正则匹配 同re.search(b,a)匹配有则成功，否则失败
+        #
+        # 注：a为匹配的正则表达式，必须字符型，b
+        # 为要匹配的内容
+        #
+        # self.assertNotRegexpMatches  # 同上，判断相反
+
+
 
 
 if __name__=="__main__":
