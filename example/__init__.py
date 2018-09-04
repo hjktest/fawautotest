e535674dae5ee44f901cf59b5af1517814ddb873
hjktest <648073731@qq.com>
2018-09-04 11:36:21 +0800

    python example2018年9月4 日

---
+++ example/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author:houjinkai
+# datetime:2018/9/4 11:32
+# software: PyCharm
---
+++ example/string.count.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author:houjinkai
+# datetime:2018/9/4 11:33
+# software: PyCharm
+str="this is string example....wow!!!"
+sub="i";
+print "str.count(sub,4,40):",str.count(sub,4,40)
+sub="wow";
+print "str.count(sub):",str.count(sub)
