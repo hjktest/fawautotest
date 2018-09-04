4e4b18a1e17cd32235484ab25b135d4b86e7faf1
hjktest <648073731@qq.com>
2018-09-04 11:22:04 +0800

    High efficiency2018年9月4 日 2018年9月4日10:46:15

--- demon/High efficiency program .py
+++ demon/High efficiency program .py
@@ -4,8 +4,9 @@
 # datetime:2018/8/31 15:02
 # software: PyCharm
 from itertools import combinations
-# for x in range(1,101):
-#     print "three"[x%3*len('three')::]+"five"[x%5*len("five")::] or x
+for x in range(1,10):
+    print "three"[x%3*len('three')::]+"five"[x%5*len("five")::] or x
+
 nfc=["packers","49ers"]
 afc=["Ravens","Patriots"]
 for teama,teamb in zip(nfc,afc):
