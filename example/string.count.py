5a2950b52d4770bb091405f3c49892c5d2eddc82
hjktest <648073731@qq.com>
2018-09-04 14:13:30 +0800

    python example2018年9月4 日

--- example/string.count.py
+++ example/string.count.py
@@ -5,6 +5,7 @@
 # software: PyCharm
 str="this is string example....wow!!!"
 sub="i";
+print "str.count(sub):",str.count(sub)
 print "str.count(sub,4,40):",str.count(sub,4,40)
 sub="wow";
 print "str.count(sub):",str.count(sub)
