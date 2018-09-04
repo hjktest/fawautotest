3c27e898e34816ce9df3ff8c5c0c70887c67d12b
hjktest <648073731@qq.com>
2018-09-04 11:55:40 +0800

    python example2018年9月4 日

---
+++ example/Python find().py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author:houjinkai
+# datetime:2018/9/4 11:40
+# software: PyCharm
+str1 = "this is string example....wow!!!";
+str2 = "exam";
+print str1.find(str2)
+print str1.find(str2,10)
+print str1.find(str2,40)
