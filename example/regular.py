91668d1501ac45a7d2317966f9a97ac8e15942cb
hjktest <648073731@qq.com>
2018-09-05 15:25:51 +0800

    正则表达式 2018年9月5 日

--- example/regular.py
+++ example/regular.py
@@ -32,3 +32,9 @@ if matchObj:
     print "search --> matchObj.group() : ", matchObj.group()
 else:
     print "No match!!"
+
+# 身份证 1102231990xxxxxxxx
+import re
+s = '1102231990xxxxxxxx'
+res = re.search('(?P<province>\d{3})(?P<city>\d{3})(?P<born_year>\d{3})',s)
+print(res.groupdict())
