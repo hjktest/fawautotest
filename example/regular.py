2fac02a7f74e0c051d2b54ee546f5b7e1e0d191c
hjktest <648073731@qq.com>
2018-09-05 15:27:43 +0800

    正则表达式 2018年9月5 日

--- example/regular.py
+++ example/regular.py
@@ -7,6 +7,20 @@ import re
 print(re.match('www','www.runnoob.com').span())
 print(re.match('com','www.runnoob.com'))
 #re.match只匹配字符串的开始，如果字符串开始不符合正则表达式，则匹配失败，函数返回None；
+# 解析:
+#
+# 首先，这是一个字符串，前面的一个 r 表示字符串为非转义的原始字符串，让编译器忽略反斜杠，也就是忽略转义字符。但是这个字符串里没有反斜杠，所以这个 r 可有可无。
+#
+#  (.*) 第一个匹配分组，.* 代表匹配除换行符之外的所有字符。
+#  (.*?) 第二个匹配分组，.*? 后面多个问号，代表非贪婪模式，也就是说只匹配符合条件的最少字符
+#  后面的一个 .* 没有括号包围，所以不是分组，匹配效果和第一个一样，但是不计入匹配结果中。
+# matchObj.group() 等同于 matchObj.group(0)，表示匹配到的完整文本字符
+#
+# matchObj.group(1) 得到第一组匹配结果，也就是(.*)匹配到的
+#
+# matchObj.group(2) 得到第二组匹配结果，也就是(.*?)匹配到的
+#
+# 因为只有匹配结果中只有两组，所以如果填 3 时会报错。
 line = "Cats are smarter than dogs"
 
 matchObj = re.match(r'(.*) are (.*?) .*', line, re.M | re.I)
