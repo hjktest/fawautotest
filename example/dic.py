0044431f46f0b5b016978897c98f8689ac1a7b63
hjktest <648073731@qq.com>
2018-09-05 10:34:28 +0800

    dictionary 用法 2018年9月5 日

--- example/dic.py
+++ example/dic.py
@@ -7,3 +7,19 @@ dict={'name':'Zara','Age':23}
 print "Start Len: %d " % len(dict)
 dict.clear()
 print "End len: %d" % len(dict)
+#浅复制
+dict1={'name':'Zara','Age':23}
+dict2=dict1.copy()
+print "New Dictinary: %s" % dict2
+print "New Dictinary: %s" % str(dict2)
+#直接赋值和 copy 的区别
+dict1={'user':'runoob','num':[1,2,3]}
+dict2=dict1# 浅拷贝: 引用对象
+dict3=dict1.copy()# 浅拷贝：深拷贝父对象（一级目录），子对象（二级目录）不拷贝，还是引用
+#修改data数据
+dict1['user']='root'
+dict1['num'].remove(3)
+#输出结果
+print dict1
+print dict2
+print dict3
