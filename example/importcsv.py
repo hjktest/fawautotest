756c6705b4961fb904f7ebd8470c371c0a682346
hjktest <648073731@qq.com>
2018-09-06 16:50:36 +0800

    pandas

--- example/importcsv.py
+++ example/importcsv.py
@@ -5,4 +5,10 @@
 # software: PyCharm
 from pandas.io.parsers import read_csv
 df=read_csv("D:\lingjian.csv")
-print "DataFramer: ", df
+# print "DataFramer: ", df
+print "Shape:",df.shape #大小
+print "Length:",len(df)  #长度
+
+print "Column Headers",df.columns #得到每列的标题
+print "Data type",df.dtypes   #得到每列数据的类型
+print "Index:",df.index#索引
