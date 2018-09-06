74710063f15571d23fe0e05ba30d78e60de6a890
hjktest <648073731@qq.com>
2018-09-06 16:12:10 +0800

    pandas

---
+++ example/pandas.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author:houjinkai
+# datetime:2018/9/6 15:54
+# software: PyCharm
+from pandas.io.parsers import read_csv
+df=read_csv("D:\lingjian.csv")
+print "DataFramer: ", df
