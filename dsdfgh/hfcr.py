db0727ab23925b714aa37ac74be9b3566be9525e
hjktest <648073731@qq.com>
2018-09-14 13:58:35 +0800

    pandas  2018年9月14日11:52:55

---
+++ dsdfgh/hfcr.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author:houjinkai
+# datetime:2018/9/14 13:18
+# software: PymCharm
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pl
+ts=pd.Series(np.random.randn(1000),index=pd.date_range('1/1/2000',periods=1000))
+ts=ts.cumsum()
+ts.plot()
--- panda/pandastest.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-# author:houjinkai
-# datetime:2018/9/14 11:52
-# software: PyCharm
